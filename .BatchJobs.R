f4c1d3c0838385dcd6efc11a79e3142e272f3efa
Damian Gola <gola@imbs.uni-luebeck.de>
2014-10-23 13:50:16 +0200

    BatchJobs config added

---
+++ .BatchJobs.R
@@ -0,0 +1,7 @@
+cluster.functions = makeClusterFunctionsTorque('/Users/DGola/templates/simple.tmpl')
+mail.start = "none"
+mail.done = "none"
+mail.error = "none"
+mail.from = ""
+mail.to = ""
+mail.control = list(smtpServer="")

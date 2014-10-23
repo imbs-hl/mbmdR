3388cb6c98aeb61a114855a64abc09bcaf4490ab
Damian Gola <gola@imbs.uni-luebeck.de>
2014-10-23 18:01:42 +0200

    added gammastep2

---
+++ R/permutations.R
@@ -0,0 +1,129 @@
+#' @title Create partial top files
+#'
+#' @description
+#' First step of parallel workflow of MB-MDR. Creates partial topfiles on multiple CPUs.
+#'
+#' @param file [\code{string}]\cr
+#'   File path of input MB-MDR file.
+#'
+#' @param trait [\code{string}]\cr
+#'   Type of trait. "binary", "continuous" or "survival".
+#'
+#' @param cpus [\code{integer}]\cr
+#'   Name for the \link{BatchJobs} \link{Registry}. Defaults to "partialTopFiles".
+#'
+#' @param reg.id [\code{string}]\cr
+#'   Name for the \link{BatchJobs} \link{Registry}. Defaults to "partialTopFiles".
+#'
+#' @param work.dir [\code{string}]\cr
+#'   Working directory for MB-MDR. Defaults to current working directory.
+#'
+#' @param reg.dir [\code{string}]\cr
+#'   Path for saving the \link{BatchJobs} \link{Registry}. Defaults to <\code{work.dir}>/registries/<\code{reg.id}>.
+#'
+#' @param topfile [\code{string}]\cr
+#'   Path of topfile. Defaults to <\code{work.dir}>/<\code{file}>.topfile.
+#'
+#' @param out.prefix [\code{string}]\cr
+#'   Path for saving the permutation files. Defaults to <\code{work.dir}>/permutations/<\code{file}>_perm.
+#'
+#' @param skip [\code{logical}]\cr
+#'   Skip creation of a new registry if a registry is found in file.dir. Defaults to TRUE.
+#'
+#' @return Vector of type \code{integer} with job ids.
+#'
+#' @export
+runPermutations <- function(file,
+                            trait,
+                            cpus,
+                            reg.id = "permutations",
+                            work.dir = getwd(),
+                            reg.dir = file.path(work.dir, "registries", reg.id),
+                            topfile = file.path(work.dir,
+                                                paste0(gsub('(.+)\\.(.*)',
+                                                            '\\1',
+                                                            basename(file)),
+                                                       ".topfile")),
+                            out.prefix = file.path(work.dir,
+                                                   "permutations",
+                                                   paste0(gsub('(.+)\\.(.*)',
+                                                               '\\1',
+                                                               basename(file)),
+                                                          "_perm")),
+                            skip = TRUE) {
+
+  assertFile(file)
+  assertChoice(trait, c("binary", "continuous", "survival"))
+  assertNumber(cpus)
+  assertString(reg.id)
+  assertDirectory(work.dir)
+  if(!testDirectory(reg.dir)) {
+    warning(paste(checkDirectory(reg.dir), "will be created!", sep = ", "))
+    dir.create(reg.dir)
+  }
+  assertDirectory(reg.dir)
+  if(!testDirectory(dirname(out.prefix))) {
+    warning(paste(checkDirectory(dirname(out.prefix)), "will be created!", sep = ", "))
+    dir.create(dirname(out.prefix))
+  }
+  assertDirectory(dirname(out.prefix))
+  assertLogical(skip)
+
+  reg <- makeRegistry(reg.id,
+                      file.dir = reg.dir,
+                      work.dir = work.dir,
+                      skip = skip)
+
+  options <- getOption("mbmdr")
+
+  p <- rep(options$p %/% cpus, cpus)
+  p[1:(options$p %% cpus)] <- p[1:(options$p %% cpus)]+1
+
+  jobs <- batchMap(reg, gammastep3,
+                   p = p, i = 1:cpus,
+                   more.args = list(file = file,
+                                    trait = trait,
+                                    t = topfile,
+                                    out.prefix = out.prefix))
+
+  submitJobs(reg, chunk(jobs, n.chunks = 1),
+             chunks.as.arrayjobs = getConfig()$ssh,
+             job.delay = TRUE)
+
+  return(reg)
+
+}
+
+gammastep3 <- function(file, trait, p, i, t, out.prefix) {
+
+  check.options()
+
+  options <- getOption("mbmdr")
+
+  cmd <- paste(options$exec,
+               " --", trait,
+               " --gammastep3",
+               " -p ", p,
+               " -t ", t,
+               " -i ", i,
+               " -o ", paste0(out.prefix, i, '.txt'),
+               ifelse(testNull(options$r), "", paste0(" -r ", options$r)),
+               " -m ", options$m,
+               " -a ", options$a,
+               ifelse(testCharacter(options$e),
+                      paste(" -e ", paste(options$e, collapse = ",")), ""),
+               ifelse(testString(options$E),
+                      paste(" -E ", options$E), ""),
+               ifelse(testCharacter(options$filter),
+                      paste(" -f ", paste(options$filter, collapse = ",")), ""),
+               ifelse(testString(options$filter.file),
+                      paste(" -F ", options$filter.file), ""),
+               " -if ", options$input.format,
+               ifelse(trait == "continuous", paste0(" -rt ", options$rt), ""),
+               " -pb ", options$pb,
+               " ", file,
+               sep = "")
+
+  system(cmd)
+
+}
--- R/topfiles.R
+++ R/topfiles.R
@@ -21,6 +21,9 @@
 #' @param reg.dir [\code{string}]\cr
 #'   Path for saving the \link{BatchJobs} \link{Registry}. Defaults to <\code{work.dir}>/registries/<\code{reg.id}>.
 #'
+#' @param out.prefix [\code{string}]\cr
+#'   Path for saving the partial topfiles. Defaults to <\code{work.dir}>/topfiles/<\code{file}>_top.
+#'
 #' @param skip [\code{logical}]\cr
 #'   Skip creation of a new registry if a registry is found in file.dir. Defaults to TRUE.
 #'
@@ -33,6 +36,12 @@ createPartialTopFiles <- function(file,
                                   reg.id = "partialTopFiles",
                                   work.dir = getwd(),
                                   reg.dir = file.path(work.dir, "registries", reg.id),
+                                  out.prefix = file.path(work.dir,
+                                                         "topfiles",
+                                                         paste0(gsub('(.+)\\.(.*)',
+                                                                     '\\1',
+                                                                     basename(file)),
+                                                                "_top")),
                                   skip = TRUE) {
 
   assertFile(file)
@@ -40,7 +49,16 @@ createPartialTopFiles <- function(file,
   assertNumber(cpus)
   assertString(reg.id)
   assertDirectory(work.dir)
-  ifelse(testDirectory(reg.dir), NULL, paste(checkDirectory(reg.dir), "will be created!", sep = ", "))
+  if(!testDirectory(reg.dir)) {
+    warning(paste(checkDirectory(reg.dir), "will be created!", sep = ", "))
+    dir.create(reg.dir)
+  }
+  assertDirectory(reg.dir)
+  if(!testDirectory(dirname(out.prefix))) {
+    warning(paste(checkDirectory(dirname(out.prefix)), "will be created!", sep = ", "))
+    dir.create(dirname(out.prefix))
+  }
+  assertDirectory(dirname(out.prefix))
   assertLogical(skip)
 
   reg <- makeRegistry(reg.id,
@@ -53,7 +71,8 @@ createPartialTopFiles <- function(file,
   jobs <- batchMap(reg, gammastep1,
                    ids, more.args = list(file = file,
                                          trait = trait,
-                                         cpus = cpus))
+                                         cpus = cpus,
+                                         ti = out.prefix))
 
   submitJobs(reg, chunk(jobs, n.chunks = 1),
              chunks.as.arrayjobs = getConfig()$ssh,
@@ -63,7 +82,98 @@ createPartialTopFiles <- function(file,
 
 }
 
-gammastep1 <- function(file, trait, id, cpus) {
+#' @title Combine partial top files
+#'
+#' @description
+#' Second step of parallel workflow of MB-MDR. Creates topfiles from multiple partial topfiles.
+#'
+#' @param file [\code{string}]\cr
+#'   File path of input MB-MDR file.
+#'
+#' @param trait [\code{string}]\cr
+#'   Type of trait. "binary", "continuous" or "survival".
+#'
+#' @param cpus [\code{integer}]\cr
+#'   Name for the \link{BatchJobs} \link{Registry}. Defaults to "partialTopFiles".
+#'
+#' @param reg.id [\code{string}]\cr
+#'   Name for the \link{BatchJobs} \link{Registry}. Defaults to "partialTopFiles".
+#'
+#' @param work.dir [\code{string}]\cr
+#'   Working directory for MB-MDR. Defaults to current working directory.
+#'
+#' @param reg.dir [\code{string}]\cr
+#'   Path for saving the \link{BatchJobs} \link{Registry}. Defaults to <\code{work.dir}>/registries/<\code{reg.id}>.
+#'
+#' @param topfiles.prefix [\code{string}]\cr
+#'   Path of partial topfiles. Defaults to <\code{work.dir}>/topfiles/<\code{file}>_top.
+#'
+#' @param out [\code{string}]\cr
+#'   Path for saving the final topfile. Defaults to <\code{work.dir}>/<\code{file}>.topfile.
+#'
+#' @param skip [\code{logical}]\cr
+#'   Skip creation of a new registry if a registry is found in file.dir. Defaults to TRUE.
+#'
+#' @return Vector of type \code{integer} with job ids.
+#'
+#' @export
+combinePartialTopFiles <- function(file,
+                                   trait,
+                                   cpus,
+                                   reg.id = "combineTopFiles",
+                                   work.dir = getwd(),
+                                   reg.dir = file.path(work.dir, "registries", reg.id),
+                                   topfiles.prefix = file.path(work.dir,
+                                                               "topfiles",
+                                                               paste0(gsub('(.+)\\.(.*)',
+                                                                           '\\1',
+                                                                           basename(file)),
+                                                                      "_top")),
+                                   out = file.path(work.dir,
+                                                          paste0(gsub('(.+)\\.(.*)',
+                                                                      '\\1',
+                                                                      basename(file)),
+                                                                 ".topfile")),
+                                   skip = TRUE) {
+
+  assertFile(file)
+  assertChoice(trait, c("binary", "continuous", "survival"))
+  assertNumber(cpus)
+  assertString(reg.id)
+  assertDirectory(work.dir)
+  if(!testDirectory(reg.dir)) {
+    warning(paste(checkDirectory(reg.dir), "will be created!", sep = ", "))
+    dir.create(reg.dir)
+  }
+  assertDirectory(reg.dir)
+  if(!testDirectory(dirname(out))) {
+    warning(paste(checkDirectory(dirname(out)), "will be created!", sep = ", "))
+    dir.create(dirname(out))
+  }
+  assertDirectory(dirname(topfiles.prefix))
+  assertDirectory(dirname(out))
+  assertLogical(skip)
+
+  reg <- makeRegistry(reg.id,
+                      file.dir = reg.dir,
+                      work.dir = work.dir,
+                      skip = skip)
+
+  jobs <- batchMap(reg, gammastep2,
+                   file, more.args = list(trait = trait,
+                                          cpus = cpus,
+                                          ti = topfiles.prefix,
+                                          t = out))
+
+  submitJobs(reg, chunk(jobs, n.chunks = 1),
+             chunks.as.arrayjobs = getConfig()$ssh,
+             job.delay = TRUE)
+
+  return(reg)
+
+}
+
+gammastep1 <- function(file, trait, id, cpus, ti) {
 
   check.options()
 
@@ -74,6 +184,7 @@ gammastep1 <- function(file, trait, id, cpus) {
                " --gammastep1",
                " -i ", id,
                " -N ", cpus,
+               " -ti ", ti,
                " -n ", options$n,
                " -m ", options$m,
                " -a ", options$a,
@@ -86,9 +197,45 @@ gammastep1 <- function(file, trait, id, cpus) {
                ifelse(testString(options$filter.file),
                       paste(" -F ", options$filter.file), ""),
                " -if ", options$input.format,
-               " -rt ", options$rt,
+               ifelse(trait == "continuous", paste0(" -rt ", options$rt), ""),
                " -pb ", options$pb,
                " ", file,
                sep = "")
+
+  system(cmd)
+
+}
+
+gammastep2 <- function(file, trait, cpus, ti, t) {
+
+  check.options()
+
+  options <- getOption("mbmdr")
+
+  cmd <- paste(options$exec,
+               " --", trait,
+               " --gammastep2",
+               " -N ", cpus,
+               " -ti ", ti,
+               " -t ", t,
+               " -n ", options$n,
+               " -m ", options$m,
+               " -a ", options$a,
+               ifelse(testCharacter(options$e),
+                      paste(" -e ", paste(options$e, collapse = ",")), ""),
+               ifelse(testString(options$E),
+                      paste(" -E ", options$E), ""),
+               ifelse(testCharacter(options$filter),
+                      paste(" -f ", paste(options$filter, collapse = ",")), ""),
+               ifelse(testString(options$filter.file),
+                      paste(" -F ", options$filter.file), ""),
+               " -v ", options$v,
+               " -if ", options$input.format,
+               ifelse(trait == "continuous", paste0(" -rt ", options$rt), ""),
+               " -pb ", options$pb,
+               " ", file,
+               sep = "")
+
+  system(cmd)
 
 }

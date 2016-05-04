d2c0010b52609936969efb35bd1b5268dd8d57bf
Damian Gola <gola@imbs.uni-luebeck.de>
2016-05-04 14:10:20 +0200

    requests one GB of memory for safety

--- R/output.R
+++ R/output.R
@@ -96,7 +96,7 @@ createOutput <- function(file,
              chunks.as.arrayjobs = getConfig()$ssh,
              resources = list(nodes = 1,
                               ppn = 1,
-                              mem = paste0(ceiling(2*file.size(file)/1024^3), "g")),
+                              mem = paste0(1+ceiling(2*file.size(file)/1024^3), "g")),
              job.delay = TRUE)
 
   return(reg)
